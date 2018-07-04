e6b530fd55c42669083519b62e947899c7e8af32
chengle123 <314737853@qq.com>
2018-07-04 13:50:11 +0800

    添加图片合成

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const Sequelize= require('sequelize');
 const qr = require('qr-image');
 const Jimp = require("jimp");
 const fs = require('fs');
+const webshot = require('webshot');
+
 
 // 本地服务======================================================================================
 
@@ -141,10 +143,11 @@ router.post('/login', function(req, res) {
 router.post('/getQrCodeImg', function(req, res) {
     var data = JSON.parse(req.body.data);
     var url = req.body.url;
+    console.log(url)
     try{
         goodsQrCodeImg.qrCode(url,(qrImg)=>{
             try{
-                goodsQrCodeImg.composite(qrImg, data.Pic, res);
+                goodsQrCodeImg.composite(qrImg, data, res);
             }catch(e) {
                 res.json({
                     result: 'error',
@@ -166,47 +169,129 @@ app.use('/', router);
 
 var goodsQrCodeImg = {
     qrCode : (url,fn)=>{
-        var qr_png = qr.image(url, { type: 'png', margin: 2, size: 8});
-        var imgName = `./images/${new Date().getTime()}.png`;
+        var qr_png = qr.image(url, { type: 'png', margin: 0, size: 8});
+        var dateName = new Date().getTime();
+        var imgName = `./images/${dateName}.png`;
         var qr_pipe = qr_png.pipe(fs.createWriteStream(imgName));
         qr_pipe.on('error', function(err){
             console.log(err);
             return;
         })
         qr_pipe.on('finish', function(){
-            // console.log(imgName);
-            fn(imgName);
+            fn(dateName);
         })
     },
-    composite: (qrImg, url, res) => {
-        var downUrl = imgDispose(url);
-        Jimp.read(qrImg, function (err, lennaQR) {
-        lennaQR.resize(232, 232).write(qrImg);
-        Jimp.read(downUrl, function (err, lennaIMG) {
-            if (err) throw err;
-            var imgName = qrImg.split('images/')[1];
-            var top = lennaIMG.bitmap.height - 242, left = lennaIMG.bitmap.width - 242;
-            lennaIMG.composite(lennaQR, left, top).write(`./images/${imgName}`);
-            return res.json({
-                result: 'success',
-                data: {
-                    imgUrl: 'http://meiguang.emym.top/images/'+imgName,
-                    imgName: imgName
-                },
-                msg: '获取图片地址成功'
-            })
+    composite: (dateName, data, res) => {
+        var downUrl = goodsQrCodeImg.imgDispose(data.Pic);
+        var imgName = `${dateName}.png`;
+        var isTmall = data.IsTmall == 1 ? './template/tmall.png' : './template/tao.png';
+        var html = `<html>
+                    <body>
+                        <div style="width:750px;height:1192px;position: relative;font-family:sans-serif, 'Microsoft YaHei';">
+                            <div style="font-size:32px;position: absolute;left: 44px;top:894px;">
+                                <div style="position: absolute; width: 350px; top: -6px; left: 45px;">${data.ShowTitle}</div>
+                            </div>
+                            <div style="color:#fff;text-align: center;width:292px;height:354px;position:absolute;left:445px;top:610px;">
+                                <div style="font-size:76px;position: relative;top: 105px;">${(data.ShowPrice - data.QuanAmount) * 100 / 10000}</div>
+                                <div style="font-size:45px;position: relative;top:135px">${data.QuanAmount * 100 / 10000}元券</div>
+                            </div>
+                            <div style="color:#d70b50;font-size: 60px; position: absolute; top: 1000px; left: 40px;">
+                                ${(data.ShowPrice - data.QuanAmount) * 100 / 10000}<span style="font-size:40px;">RMB</span>
+                                <span style="text-decoration: line-through;color:#919191;font-size:30px;">
+                                    ${data.ShowPrice * 100 / 10000}RMB
+                                </span>
+                            </div>
+                            <div style="color:#919191;font-size:18px;position: absolute; top: 1105px; left: 50px;">
+                                ${data.SealCount}人购买
+                            </div>
+                        </div>
+                    </body>
+                    </html>`;
+        webshot(html, `./images/${dateName}text.png`, {siteType:'html',screenSize: {width: 750, height: 1192}, shotSize: {width: 750, height: 1192}}, function(err) {
+            Jimp.read('./template/spfxdtnew.png', (err,bgImg) => {
+                Jimp.read(downUrl, function (err, lennaIMG) {
+                    lennaIMG.resize(701, 701);
+                    Jimp.read('./template/dunpai.png', function (err, dunpai) {
+                        dunpai.resize(292, 354);
+                        Jimp.read(isTmall, function (err, isTmallImg) {
+                            isTmallImg.resize(33, 33);
+                            Jimp.read(`./images/${dateName}.png`, function (err, lennaQR) {
+                                lennaQR.resize(130, 130);
+                                Jimp.read(`./images/${dateName}text.png`, function (err, textImg) {
+                                    bgImg.composite(lennaIMG, 26, 151)
+                                            .composite(lennaQR, 529, 981)
+                                            .composite(dunpai, 445, 610)
+                                            .composite(isTmallImg, 44, 894)
+                                            .composite(textImg, 0, 0)
+                                            .write(`./images/${imgName}`);
+                                    return res.json({
+                                        result: 'success',
+                                        data: {
+                                            imgUrl: 'http://meiguang.emym.top/images/'+imgName,
+                                            imgName: imgName
+                                        },
+                                        msg: '获取图片地址成功'
+                                    })
+                                });
+                            });
+                        });
+                    });
+                });
+            });
         });
-        });
+    },
+    imgDispose: url => {
+        if(url.indexOf('_.webp')>-1){
+            return url.split('_.webp')[0];
+        }else{
+            return url;
+        }
     }
-}
+};
+
+// var goodsQrCodeImg = {
+//     qrCode : (url,fn)=>{
+//         var qr_png = qr.image(url, { type: 'png', margin: 2, size: 8});
+//         var imgName = `./images/${new Date().getTime()}.png`;
+//         var qr_pipe = qr_png.pipe(fs.createWriteStream(imgName));
+//         qr_pipe.on('error', function(err){
+//             console.log(err);
+//             return;
+//         })
+//         qr_pipe.on('finish', function(){
+//             // console.log(imgName);
+//             fn(imgName);
+//         })
+//     },
+//     composite: (qrImg, url, res) => {
+//         var downUrl = imgDispose(url);
+//         Jimp.read(qrImg, function (err, lennaQR) {
+//         lennaQR.resize(232, 232).write(qrImg);
+//         Jimp.read(downUrl, function (err, lennaIMG) {
+//             if (err) throw err;
+//             var imgName = qrImg.split('images/')[1];
+//             var top = lennaIMG.bitmap.height - 242, left = lennaIMG.bitmap.width - 242;
+//             lennaIMG.composite(lennaQR, left, top).write(`./images/${imgName}`);
+//             return res.json({
+//                 result: 'success',
+//                 data: {
+//                     imgUrl: 'http://meiguang.emym.top/images/'+imgName,
+//                     imgName: imgName
+//                 },
+//                 msg: '获取图片地址成功'
+//             })
+//         });
+//         });
+//     }
+// }
   
-function imgDispose(url){
-  if(url.indexOf('_.webp')>-1){
-    return url.split('_.webp')[0];
-  }else{
-    return url;
-  }
-}
+// function imgDispose(url){
+//   if(url.indexOf('_.webp')>-1){
+//     return url.split('_.webp')[0];
+//   }else{
+//     return url;
+//   }
+// }
 
 // 定时更新任务
 cron.schedule('1 0 0 * * *', function() {
